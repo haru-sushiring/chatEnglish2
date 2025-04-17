39c5848b8a31bb79e23f54851c994164bbfcad11
haru-sushiring <harukiblog01@gmail.com>
2025-04-18 01:49:42 +0900

    NEXT_PUBLIC_API_ROOTの修正

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_ROOT: process.env.NEXT_PUBLIC_API_ROOT,
+  },
+};
 
 export default nextConfig;
