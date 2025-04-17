b3f0fab740da906776df50c964d8fd9869ead2d8
haru-sushiring <harukiblog01@gmail.com>
2025-04-18 08:25:03 +0900

    NEXT_PUBLIC_APP_ROOTが反応しないので一旦ハードコーディングで対応

--- src/src/utils/http.ts
+++ src/src/utils/http.ts
@@ -1,11 +1,15 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, HttpStatusCode } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_ROOT}`,
+  baseURL: window.location.origin === 'https://client-zjom.onrender.com'
+    ? 'https://client-zjom.onrender.com'
+    : `${process.env.NEXT_PUBLIC_API_ROOT}`,
   headers: {
     Accept: 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
-    'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_APP_ROOT}`,
+    'Access-Control-Allow-Origin': window.location.origin === 'https://client-zjom.onrender.com'
+      ? 'https://client-zjom.onrender.com'
+      : `${process.env.NEXT_PUBLIC_APP_ROOT}`,
     'content-type': 'application/json;charset=utf-8',
   },
   withCredentials: true,
